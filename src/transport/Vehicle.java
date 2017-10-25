285e337d573eace82e1c6da0a1c01122291b2b5d
David Rojas Julia <drj00@iesemilidarder.com>
2017-10-25 20:52:20 +0200

    Initial commit

---
+++ src/transport/Vehicle.java
@@ -0,0 +1,44 @@
+package transport;
+
+public class Vehicle {
+    /**
+     * Metodos utilizados
+     */
+
+    private double load;
+    private double maxLoad;
+
+    public Vehicle(double maxLoad) {
+        this.maxLoad = maxLoad;
+    }
+
+    public double getLoad() {
+        return load;
+    }
+
+    public double getMaxLoad() {
+        return maxLoad;
+    }
+
+    /**
+     * Aqui declaramos las cargas del camion
+     */
+
+    public boolean addBox(double weight) {
+
+        if (this.load + weight < maxLoad) {
+            this.load = weight + this.load;
+            System.out.println("Cargamos al caminon " +weight + " KG");
+            return true;
+        } else {
+            System.out.println("Carga no introducida " +weight + " KG");
+            return false;
+        }
+    }
+}
+
+
+/**
+ * David Rojas Julia
+ * 2º ASIX
+ */
