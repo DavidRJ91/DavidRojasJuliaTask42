0804e7b25737e7fed3727ad4ded0fadc95761d4b
David Rojas Julia <32340635+DavidRJ91@users.noreply.github.com>
2017-10-28 19:39:06 +0200

    Update Road.java

--- src/transport/Road.java
+++ src/transport/Road.java
@@ -1,4 +1,41 @@
 package transport;
 
+
 public class Road {
+    private Vehicle[] Nvehicles;
+    private int numberOfVehicles;
+
+    public Road() {
+        this.Nvehicles = new Vehicle[5];
+        numberOfVehicles = 0;
+        System.out.println("Hay " + getNumberOfVehicles() + " camiones en la carretera.");
+
+    }
+
+    public boolean addVehicle (double MaxLoad){
+        if (numberOfVehicles < this.Nvehicles.length){
+            Nvehicles [numberOfVehicles] = new Vehicle(MaxLoad);
+            numberOfVehicles++;
+            System.out.println("El camion numero "+numberOfVehicles+" con "+MaxLoad+" KG de carga");
+
+            return true;
+        }
+        else{
+            System.out.println("ERROR: En esta carretera solo pueden ir 5 vehiculos.");
+            return false;
+        }
+    }
+
+    public int getNumberOfVehicles (){
+        return numberOfVehicles;
+    }
+
+    public Vehicle getVehicle(int j){
+        if(j < Nvehicles.length) {
+            return Nvehicles[j];
+        }
+        else{
+            return null;
+        }
+    }
 }
