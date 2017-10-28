adb5635b7f695f1bd38f4286e92fddb92ef39bff
David Rojas Julia <32340635+DavidRJ91@users.noreply.github.com>
2017-10-28 19:39:30 +0200

    Update Road_test.java

--- src/transport/Road_test.java
+++ src/transport/Road_test.java
@@ -1,4 +1,13 @@
 package transport;
 
 public class Road_test {
+    public static void main(String[] args) {
+        Road myRoad = new Road();
+        myRoad.addVehicle(10000);
+        myRoad.addVehicle(5000);
+        myRoad.addVehicle(4000);
+        myRoad.addVehicle(20000);
+        myRoad.addVehicle(15000);
+        myRoad.addVehicle(4000);
+    }
 }
